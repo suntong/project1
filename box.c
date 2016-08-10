a913ffa28cb4096e8595f74b646dc0d0c4e6749a
doguri <doguri@Davids-MacBook-Air.local>
2016-08-10 12:29:24 -0700

    fixed box.c

--- box.c
+++ box.c
@@ -2,8 +2,8 @@
 #include<stdlib.h>
 
 int main(int argc, char *argv[]) {
-  int rows=10;
-  int cols=10;
+  int rows=9;
+  int cols=9;
   //
   if (argc == 2) {
     rows=atoi(argv[1]);
@@ -12,6 +12,8 @@ int main(int argc, char *argv[]) {
   //
   int r;
   int c;
+  rows=rows+1;
+  cols=cols+1;
   for (r=1;r<rows;r=r+1) {
     for (c=1;c<cols;c=c+1) {
       if ((r==1) || (r==rows-1) || (c==1) || (c==cols-1) || (r==c) || (rows-r==c)) {
